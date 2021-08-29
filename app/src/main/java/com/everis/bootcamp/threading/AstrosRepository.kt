8b6641d44f7c657ef3ff5664dff9179cd43f388a
tonin <antonio.leite00@hotmail.com>
2021-08-28 21:43:25 -0300

    Consumida a API e feita a conversão

--- app/src/main/java/com/everis/bootcamp/threading/AstrosRepository.kt
+++ app/src/main/java/com/everis/bootcamp/threading/AstrosRepository.kt
@@ -1,10 +1,31 @@
 package com.everis.bootcamp.threading
 
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-//TODO: 010 - Criar a classe responsável por carregar os dados
+
 class AstrosRepository {
 
-    //TODO: 011 - Criar função para carregar os astronautas
 
-    //TODO: 012 - Criar função para converter o json
+    fun loadData() : List<AstrosPeople>{
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("http://api.open-notify.org/astros.json")
+            .build()
+
+        val response = client.newCall(request).execute()
+
+        val result = parseJsonToResult(response.body?.string())
+
+        return result.people
+
+    }
+
+
+
+    fun parseJsonToResult(json : String?) =
+        Gson().fromJson(json, AstrosResult::class.java)
+
 }
